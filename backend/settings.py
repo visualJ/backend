8fd4955586f9ad7cf88e068668be46a8c505d4d6
Jonas Knorr <sieg.tronje@gmail.com>
2017-11-22 10:02:21 +0100

    Add restframeworkcml to requirements

--- backend/settings.py
+++ backend/settings.py
@@ -25,14 +25,14 @@ SECRET_KEY = '=a@f-9u12i8odtf69y+#mc6y_wjiwjwoylr+ky4#ke&q6*#8!h'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '.elasticbeanstalk.com'
+    '.elasticbeanstalk.com',
+    'localhost'
 ]
 
 # Application definition
 
 INSTALLED_APPS = [
     'rest_framework',
-    'rest_framework_xml',
     'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
